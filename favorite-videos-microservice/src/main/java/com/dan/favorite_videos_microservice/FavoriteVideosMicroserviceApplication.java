f24e4cf77414fc824407c6c8e416f4fe8287cf44
Dan <daniel_vazrobk@hotmail.com>
2025-04-01 16:59:50 -0600

    Primer commit

---
+++ favorite-videos-microservice/src/main/java/com/dan/favorite_videos_microservice/FavoriteVideosMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.dan.favorite_videos_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteVideosMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteVideosMicroserviceApplication.class, args);
+	}
+
+}
---
+++ favorite-videos-microservice/src/test/java/com/dan/favorite_videos_microservice/FavoriteVideosMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dan.favorite_videos_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteVideosMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
