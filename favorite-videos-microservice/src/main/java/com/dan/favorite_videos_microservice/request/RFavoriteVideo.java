df594c4fb0102d85df61ef4578be92516b5f7d1c
Dan <daniel_vazrobk@hotmail.com>
2025-04-02 17:24:10 -0600

    Se agrego al record RFavoriteVideo las anotaciones @NotNull y @NotBlank a sus atributos

--- favorite-videos-microservice/src/main/java/com/dan/favorite_videos_microservice/request/RFavoriteVideo.java
+++ favorite-videos-microservice/src/main/java/com/dan/favorite_videos_microservice/request/RFavoriteVideo.java
@@ -2,6 +2,20 @@ package com.dan.favorite_videos_microservice.request;
 
 import java.util.UUID;
 
-public record RFavoriteVideo(UUID userId, String videoId, String title, String url) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RFavoriteVideo(
+		@NotNull(message = "Por favor especifica el id del usuario.")
+		UUID userId,
+		
+		@NotBlank(message = "Por favor especifica el id del video.")
+		String videoId,
+		
+		@NotBlank(message = "Por favor especifica el título del video.")
+		String title,
+		
+		@NotBlank(message = "Por favor especifica la url imagen del video.")
+		String url) {
 
 }
