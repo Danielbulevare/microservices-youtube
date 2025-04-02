c476059a39cea65605400fe661629a0f88b186f6
Dan <daniel_vazrobk@hotmail.com>
2025-04-02 15:48:30 -0600

    Se modifico el tipo de retorno del metodo saveVideo

--- favorite-videos-microservice/src/main/java/com/dan/favorite_videos_microservice/service/IFavoriteVideoService.java
+++ favorite-videos-microservice/src/main/java/com/dan/favorite_videos_microservice/service/IFavoriteVideoService.java
@@ -2,9 +2,8 @@ package com.dan.favorite_videos_microservice.service;
 
 import java.util.Optional;
 
-import com.dan.favorite_videos_microservice.request.RFavoriteVideo;
-import com.dan.favorite_videos_microservice.responses.IResponseFavoriteVideoRepositoryId;
+import com.dan.favorite_videos_microservice.entities.CFavoriteVideo;
 
-public interface IFavoriteVideoRepositoryService {
-	Optional<IResponseFavoriteVideoRepositoryId> saveVideo(RFavoriteVideo favoriteVideo);
+public interface IFavoriteVideoService {
+	Optional<CFavoriteVideo> saveVideo(CFavoriteVideo favoriteVideo);
 }
