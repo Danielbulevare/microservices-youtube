e3472209a7852047eb33175d211257a15f84c1b5
Dan <daniel_vazrobk@hotmail.com>
2025-04-02 16:33:57 -0600

    Se elimino del record RFavoriteVideo el atributo id

--- favorite-videos-microservice/src/main/java/com/dan/favorite_videos_microservice/request/RFavoriteVideo.java
+++ favorite-videos-microservice/src/main/java/com/dan/favorite_videos_microservice/request/RFavoriteVideo.java
@@ -2,6 +2,6 @@ package com.dan.favorite_videos_microservice.request;
 
 import java.util.UUID;
 
-public record RFavoriteVideo(UUID id, String videoId, String title, String url) {
+public record RFavoriteVideo(UUID userId, String videoId, String title, String url) {
 
 }
