af19e64778b1ac8899b54bb4177251225fd4b98f
Dan <daniel_vazrobk@hotmail.com>
2025-04-02 15:19:34 -0600

    Se agrego la anotacion @Repository a la interfaz IFavoriteRepository

--- favorite-videos-microservice/src/main/java/com/dan/favorite_videos_microservice/repository/IFavoriteRepository.java
+++ favorite-videos-microservice/src/main/java/com/dan/favorite_videos_microservice/repository/IFavoriteRepository.java
@@ -3,9 +3,11 @@ package com.dan.favorite_videos_microservice.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.dan.favorite_videos_microservice.entities.CFavoriteVideos;
 
+@Repository
 public interface IFavoriteRepository extends JpaRepository<CFavoriteVideos, UUID>{
 
 }
