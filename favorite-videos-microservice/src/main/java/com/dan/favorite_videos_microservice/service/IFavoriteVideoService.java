f5e09d85ba5a6771ee08da31a1b206e43aa64f7f
Dan <daniel_vazrobk@hotmail.com>
2025-04-02 17:28:23 -0600

    Se agrego al metodo saveVideo de la interfaz IFavoriteVideoService el menejo de la excepcion CVideoAlreadyExistException

--- favorite-videos-microservice/src/main/java/com/dan/favorite_videos_microservice/service/IFavoriteVideoService.java
+++ favorite-videos-microservice/src/main/java/com/dan/favorite_videos_microservice/service/IFavoriteVideoService.java
@@ -3,7 +3,8 @@ package com.dan.favorite_videos_microservice.service;
 import java.util.Optional;
 
 import com.dan.favorite_videos_microservice.entities.CFavoriteVideo;
+import com.dan.favorite_videos_microservice.error.CVideoAlreadyExistException;
 
 public interface IFavoriteVideoService {
-	Optional<CFavoriteVideo> saveVideo(CFavoriteVideo favoriteVideo);
+	Optional<CFavoriteVideo> saveVideo(CFavoriteVideo favoriteVideo) throws CVideoAlreadyExistException;
 }
