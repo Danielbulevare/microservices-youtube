3684e9659c89f728f7dc650c9cf0c7baa9d19f90
Dan <daniel_vazrobk@hotmail.com>
2025-04-02 18:23:14 -0600

    Se declararon los metodos findUserVideos y totalUserVideos en la interfaz

--- favorite-videos-microservice/src/main/java/com/dan/favorite_videos_microservice/service/IFavoriteVideoService.java
+++ favorite-videos-microservice/src/main/java/com/dan/favorite_videos_microservice/service/IFavoriteVideoService.java
@@ -1,10 +1,14 @@
 package com.dan.favorite_videos_microservice.service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import com.dan.favorite_videos_microservice.entities.CFavoriteVideo;
 import com.dan.favorite_videos_microservice.error.CVideoAlreadyExistException;
 
 public interface IFavoriteVideoService {
 	Optional<CFavoriteVideo> saveVideo(CFavoriteVideo favoriteVideo) throws CVideoAlreadyExistException;
+	List<CFavoriteVideo> findUserVideos(UUID userId, int page, int records);
+	long totalUserVideos(UUID userId);
 }
