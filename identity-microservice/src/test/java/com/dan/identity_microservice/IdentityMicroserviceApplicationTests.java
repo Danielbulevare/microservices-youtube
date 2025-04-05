f067d8094b7eee61e1046edb5e7457a479f67852
Dan <daniel_vazrobk@hotmail.com>
2025-04-05 16:07:25 -0600

    Se agrego el modulo identity-microservice en el pom del proyecto padre

---
+++ identity-microservice/src/test/java/com/dan/identity_microservice/IdentityMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dan.identity_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
