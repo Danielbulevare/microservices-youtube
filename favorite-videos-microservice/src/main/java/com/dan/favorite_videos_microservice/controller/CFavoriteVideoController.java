48ffd8f538faa11890ecf65baccd32aa350694ec
Dan <daniel_vazrobk@hotmail.com>
2025-04-05 14:46:35 -0600

    Se quitaron todas las confoguraciones de Keycloak

--- favorite-videos-microservice/src/main/java/com/dan/favorite_videos_microservice/controller/CFavoriteVideoController.java
+++ favorite-videos-microservice/src/main/java/com/dan/favorite_videos_microservice/controller/CFavoriteVideoController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.dan.favorite_videos_microservice.entities.CFavoriteVideo;
 import com.dan.favorite_videos_microservice.error.CVideoAlreadyExistException;
@@ -30,7 +29,6 @@ public class CFavoriteVideoController {
 	private IFavoriteVideoService favoriteVideoService;
 
 	@PostMapping
-	@PreAuthorize("hasRole('ROLE_USER')")
 	@ResponseStatus(HttpStatus.OK)
 	public Optional<CFavoriteVideo> saveVideo(@Valid @RequestBody RFavoriteVideo favoriteVideo)
 			throws CVideoAlreadyExistException {
@@ -39,7 +37,6 @@ public class CFavoriteVideoController {
 	}
 
 	@GetMapping("/{userId}/{page}/{records}")
-	@PreAuthorize("hasRole('ROLE_USER')")
 	@ResponseStatus(HttpStatus.OK)
 	public List<CFavoriteVideo> findUserVideos(@PathVariable UUID userId, @PathVariable int page,
 			@PathVariable int records) {
@@ -47,14 +44,12 @@ public class CFavoriteVideoController {
 	}
 	
 	@GetMapping("/{userId}")
-	@PreAuthorize("hasRole('ROLE_USER')")
 	@ResponseStatus(HttpStatus.OK)
 	public long totalUserVideos(@PathVariable UUID userId) {
 		return favoriteVideoService.totalUserVideos(userId);
 	}
 	
 	@DeleteMapping("/{id}")
-	@PreAuthorize("hasRole('ROLE_USER')")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteFavoriteVideo(@PathVariable UUID id) {
 		favoriteVideoService.deleteFavoriteVideo(id);
