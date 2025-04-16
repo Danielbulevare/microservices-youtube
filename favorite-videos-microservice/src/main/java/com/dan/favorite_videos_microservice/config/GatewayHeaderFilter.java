b466f7b3c96cb803a3f3dc3100e3aad0db99820a
Dan <daniel_vazrobk@hotmail.com>
2025-04-15 18:31:52 -0600

    Se implemento seguridad en el microservicio para que las solicitudes solo dean por medio del Api Gateway

---
+++ favorite-videos-microservice/src/main/java/com/dan/favorite_videos_microservice/config/GatewayHeaderFilter.java
@@ -0,0 +1,33 @@
+package com.dan.favorite_videos_microservice.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class GatewayHeaderFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String forwardedHost = request.getHeader("X-Forwarded-Host");
+
+        // Permite solo si la petición viene del API Gateway
+        if (forwardedHost == null || !forwardedHost.contains("localhost:8080")) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "⛔ Acceso solo permitido desde el API Gateway");
+            return;
+        }
+
+        // Si el header existe, continúa con el flujo normal
+        filterChain.doFilter(request, response);
+    }
+}
--- identity-microservice/src/main/java/com/dan/identity_microservice/service/CJwtService.java
+++ identity-microservice/src/main/java/com/dan/identity_microservice/service/CJwtService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
